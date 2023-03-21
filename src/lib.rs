608d455a3397178b62215735b301f08860d7b8be
devin12422 <kramerdevin321@gmail.com>
2023-03-21 18:07:04 -0400

    bobb

--- src/lib.rs
+++ src/lib.rs
@@ -16,74 +16,94 @@ mod core {
             window::{Window, WindowBuilder},
         };
 
-        pub trait Renderer: Send + Sync {
-            // fn new(window: Window) -> impl Future<Output = Arc<Self>> + Send;
-            async fn new(window: Window) -> Self;
-            fn render(&mut self) -> Result<(), wgpu::SurfaceError>;
+        pub trait Renderer: Send{
+            fn new(window: Window) -> impl Future<Output = Self> + Send ;
+            // async fn new(window: Window) -> Self;
+            fn render(&mut self) -> impl Future<Output = Result<(), wgpu::SurfaceError>>+ Send + '_;
             fn resize(&mut self, new_size: Option<winit::dpi::PhysicalSize<u32>>);
             fn get_size(&self) -> winit::dpi::PhysicalSize<u32>;
             fn get_window(&self) -> &Window;
         }
 
-        struct WGPUInterface<GenericRenderer>
-        where
-            GenericRenderer: Renderer,
+        struct WGPUInterface
         {
             surface: wgpu::Surface,
             device: wgpu::Device,
             queue: wgpu::Queue,
             config: wgpu::SurfaceConfiguration,
             size: winit::dpi::PhysicalSize<u32>,
-            renderer: GenericRenderer,
-            // renderer: Arc<GenericRenderer>, // window: Window,d
         }
         struct Instance {}
         struct RT<GenericRenderer>
         where
-            GenericRenderer: Renderer + 'static,
+            GenericRenderer: Renderer ,
         {
-            runtime: tokio::runtime::Runtime,
             renderer: GenericRenderer,
             event_loop: EventLoop<()>,
             // instance: Instance,
         }
-        pub struct RTCreateInfo<GenericRenderer>
-        where
-            GenericRenderer: Renderer + 'static,
-        {
-            runtime: tokio::runtime::Runtime,
-            renderer: GenericRenderer,
-            event_loop: EventLoop<()>,
-        }
-
+      
         impl<GenericRenderer> RT<GenericRenderer>
         where
             GenericRenderer: Renderer + 'static,
         {
-            fn new(create_info: RTCreateInfo<GenericRenderer>) -> Self {
-                let RTCreateInfo {
-                    runtime,
-                    renderer,
-                    event_loop,
-                } = create_info;
-                Self {
-                    runtime,
-                    renderer,
-                    event_loop,
+            fn new() -> impl Future<Output = Self> {
+                // #[cfg(feature = "full")]
+                // let runtime = tokio::runtime::Builder::new_multi_thread().build().unwrap();
+                // #[cfg(not(feature = "full"))]
+                // let runtime = tokio::runtime::Builder::new_current_thread()
+                //     .build()
+                //     .unwrap();
+
+                async{
+                    cfg_if::cfg_if! {
+                        if #[cfg(target_arch = "wasm32")]{
+                            std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+                            console_log::init_with_level(log::Level::Warn).expect("couldnt initialize logger");
+                        }else{
+                            env_logger::init();
+                        }
+                    }
+                    let event_loop = EventLoop::new();
+                    let window = WindowBuilder::new().build(&event_loop).unwrap();
+                    #[cfg(target_arch = "wasm32")]
+                    {
+                        use winit::dpi::PhysicalSize;
+                        window.set_inner_size(PhysicalSize::new(450, 400));
+
+                        use winit::platform::web::WindowExtWebSys;
+                        web_sys::window()
+                            .and_then(|win| win.document())
+                            .and_then(|doc| {
+                                let dst = doc.get_element_by_id("wasm-example")?;
+                                let canvas = web_sys::Element::from(window.canvas());
+                                dst.append_child(&canvas).ok()?;
+                                Some(())
+                            })
+                            .expect("Couldn't append canvas to document body.");
+                    }
+                    let renderer = task::spawn(GenericRenderer::new(window));
+                    
+                    // let instance = Instance {};
+                    // let renderer = renderer.await.unwrap();
+                    // let renderer = GenericRenderer::new(window.clone()).await;
+                    Self{renderer:renderer.await.unwrap(),event_loop}
                 }
-            }
+                          }
             fn run(mut self) {
                 let RT {
-                    mut runtime,
+                    // mut runtime,
                     mut renderer,
                     mut event_loop,
                 } = self;
 
                 // runtime.block_on(async move {
                     // runtime.spawn_blocking(move || {
+                // task::spawn(async{
                     let mut time = std::time::Instant::now();
                     let mut dt = std::time::Duration::ZERO;
-                    event_loop.run(move |event, __, control_flow| {
+                let test =                     
+move |event, _, control_flow| {
                         dt = std::time::Instant::now() - time;
                         time += dt;
                         println!("{:?}", dt);
@@ -120,139 +140,87 @@ mod core {
                                 Event::RedrawRequested(window_id)
                                     if window_id == renderer.get_window().id() =>
                                 {
-                                    // runtime.spawn(async {
-                                    match renderer.render() {
+                                    task::spawn(async{
+                                    match renderer.render().await {
                                         Ok(_) => {}
                                         Err(wgpu::SurfaceError::Lost) => {
                                             renderer.resize(None)
                                         }
                                         Err(wgpu::SurfaceError::OutOfMemory) => {
-                                            *control_flow = ControlFlow::Exit
+                                            // *control_flow = ControlFlow::Exit;:w
                                         }
                                         Err(e) => eprintln!("{:?}", e),
                                     }
+                            });
                                 }
                                 _ => {}
-                            }
+                            };
+                
+                    event_loop.run(test);
                     // });
-                    // });
-                });
+                // });
             }
         }
-        impl<GenericRenderer> Default for RTCreateInfo<GenericRenderer>
-        where
-            GenericRenderer: Renderer, // GenericRenderer: Renderer + 'static,
-        {
-            fn default() -> Self {
-                #[cfg(feature = "full")]
-                let runtime = tokio::runtime::Builder::new_multi_thread().build().unwrap();
-                #[cfg(not(feature = "full"))]
-                let runtime = tokio::runtime::Builder::new_current_thread()
-                    .build()
-                    .unwrap();
+       
+        impl  WGPUInterface        {
+            // fn new(window: Window) -> impl Future<Output = Self> + Send + 'static {
+            //     async{
+            //     let size = window.inner_size();
 
-                let result = runtime.block_on(async move{
-                    cfg_if::cfg_if! {
-                        if #[cfg(target_arch = "wasm32")]{
-                            std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-                            console_log::init_with_level(log::Level::Warn).expect("couldnt initialize logger");
-                        }else{
-                            env_logger::init();
-                        }
-                    }
-                    let event_loop = EventLoop::new();
-                    let window = WindowBuilder::new().build(&event_loop).unwrap();
-                    #[cfg(target_arch = "wasm32")]
-                    {
-                        use winit::dpi::PhysicalSize;
-                        window.set_inner_size(PhysicalSize::new(450, 400));
+            //     let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            //         backends: wgpu::Backends::all(),
+            //         ..Default::default()
+            //     });
 
-                        use winit::platform::web::WindowExtWebSys;
-                        web_sys::window()
-                            .and_then(|win| win.document())
-                            .and_then(|doc| {
-                                let dst = doc.get_element_by_id("wasm-example")?;
-                                let canvas = web_sys::Element::from(window.canvas());
-                                dst.append_child(&canvas).ok()?;
-                                Some(())
-                            })
-                            .expect("Couldn't append canvas to document body.");
-                    }
-                    let renderer = task::spawn_blocking(||{GenericRenderer::new(window)});
-                    
-                    // let instance = Instance {};
-                    // let renderer = renderer.await.unwrap();
-                    // let renderer = GenericRenderer::new(window.clone()).await;
-                    (renderer.await,event_loop)
-                });
-                RTCreateInfo {
-                    runtime,
-                    renderer: result.0,
-                    event_loop: result.1,
-                }
-            }
-        }
+            //     let surface = unsafe { instance.create_surface(&window) }.unwrap();
+            //     let adapter = instance
+            //         .request_adapter(&wgpu::RequestAdapterOptions {
+            //             power_preference: wgpu::PowerPreference::default(),
+            //             compatible_surface: Some(&surface),
+            //             force_fallback_adapter: false,
+            //         })
+            //         .await
+            //         .unwrap();
 
-        impl<GenericRenderer> Renderer for WGPUInterface<GenericRenderer>
-        where
-            GenericRenderer: Renderer,
-        {
-            async fn new(window: Window) -> Self {
-                let size = window.inner_size();
-
-                let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
-                    backends: wgpu::Backends::all(),
-                    ..Default::default()
-                });
-
-                let surface = unsafe { instance.create_surface(&window) }.unwrap();
-                let adapter = instance
-                    .request_adapter(&wgpu::RequestAdapterOptions {
-                        power_preference: wgpu::PowerPreference::default(),
-                        compatible_surface: Some(&surface),
-                        force_fallback_adapter: false,
-                    })
-                    .await
-                    .unwrap();
-
-                let (device, queue) = adapter
-                    .request_device(
-                        &wgpu::DeviceDescriptor {
-                            features: wgpu::Features::empty(),
-                            limits: if cfg!(target_arch = "wasm32") {
-                                wgpu::Limits::downlevel_webgl2_defaults()
-                            } else {
-                                wgpu::Limits::default()
-                            },
-                            label: None,
-                        },
-                        None,
-                    )
-                    .await
-                    .unwrap();
-                let config = wgpu::SurfaceConfiguration {
-                    usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-                    format: surface.get_capabilities(&adapter).formats[0],
-                    view_formats: surface.get_capabilities(&adapter).formats,
-                    width: size.width,
-                    height: size.height,
-                    present_mode: wgpu::PresentMode::Fifo,
-                    alpha_mode: wgpu::CompositeAlphaMode::Auto,
-                };
-                surface.configure(&device, &config);
-                let renderer = GenericRenderer::new(window).await;
-                Self {
-                    surface,
-                    device,
-                    queue,
-                    config,
-                    size,
-                    renderer,
-                }
-            }
-            fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
-                self.renderer.render()
-            }
+            //     let (device, queue) = adapter
+            //         .request_device(
+            //             &wgpu::DeviceDescriptor {
+            //                 features: wgpu::Features::empty(),
+            //                 limits: if cfg!(target_arch = "wasm32") {
+            //                     wgpu::Limits::downlevel_webgl2_defaults()
+            //                 } else {
+            //                     wgpu::Limits::default()
+            //                 },
+            //                 label: None,
+            //             },
+            //             None,
+            //         )
+            //         .await
+            //         .unwrap();
+            //     let config = wgpu::SurfaceConfiguration {
+            //         usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            //         format: surface.get_capabilities(&adapter).formats[0],
+            //         view_formats: surface.get_capabilities(&adapter).formats,
+            //         width: size.width,
+            //         height: size.height,
+            //         present_mode: wgpu::PresentMode::Fifo,
+            //         alpha_mode: wgpu::CompositeAlphaMode::Auto,
+            //     };
+            //     surface.configure(&device, &config);
+                
+            //     Self {
+            //         surface,
+            //         device,
+            //         queue,
+            //         config,
+            //         size,
+            //     }
+            // }}
+            // fn render(&mut self) -> impl Future<Output = Result<(), wgpu::SurfaceError>> {
+            //     async{
+            //         self.renderer.render().await
+            //     }
+            // }
 
             fn resize(&mut self, new_size: Option<winit::dpi::PhysicalSize<u32>>) {
                 let real_size = new_size.unwrap_or(self.size);
@@ -261,15 +229,15 @@ mod core {
                     self.config.width = real_size.width;
                     self.config.height = real_size.height;
                     self.surface.configure(&self.device, &self.config);
-                    self.renderer.resize(new_size);
+                    // self.renderer.resize(new_size);
                 }
             }
             fn get_size(&self) -> winit::dpi::PhysicalSize<u32> {
                 self.size.clone()
             }
-            fn get_window(&self) -> &Window {
-                self.renderer.get_window()
-            }
+            // fn get_window(&self) -> &Window {
+            //     self.renderer.get_window()
+            // }
         }
         impl Instance {
             fn input(&mut self, event: &WindowEvent) -> bool {
