38f1ff69461acbd5d793bf43b8f3550452c6b4cc
devin12422 <kramerdevin321@gmail.com>
2023-03-30 21:02:44 -0400

    yeooo

--- examples/winit/main.rs
+++ examples/winit/main.rs
@@ -20,37 +20,9 @@ use winit::{
 enum RendererInput {
     Render,
     Resize([u32; 2]),
+    Exit
 }
 use std::pin::Pin;
-
-trait AsyncFnMut<T> {
-    type Fut: Future<Output = Self::Output>;
-    type Output;
-
-    fn call(&mut self, arg: T) -> Self::Fut;
-}
-
-impl<F, Fut, T> AsyncFnMut<T> for F
-where
-    F: FnMut(T) -> Fut,
-    Fut: Future,
-{
-    type Fut = Fut;
-    type Output = Fut::Output;
-
-    fn call(&mut self, arg: T) -> Fut {
-        (self)(arg)
-    }
-}
-// trait Systemic
-//  where
-//      Self: Send + 'static,
-//  {
-// /
-//      type SystemicFuture:Future<Output = Pin<Box<Self::SystemicFuture>>> + Send + 'static;
-//      fn run() -> Pin<Box< Self::SystemicFuture>>;
-// impl for<'a> AsyncFnMut<&'a mut File, Output = std::io::Result<()>>,
-//  }
 #[repr(C)]
 #[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
 struct Vertex {
@@ -75,8 +47,6 @@ struct Renderer
     num_vertices: u32,
     index_buffer: wgpu::Buffer,
     num_indices: u32,
-    rx: Receiver<RendererInput>,
-    notify: Arc<Notify>,
 }
 impl WindowAbstractor for WindowHandler {
     fn get_size(&self) -> winit::dpi::PhysicalSize<u32> {
@@ -112,13 +82,8 @@ use tokio::sync::watch::*;
 // }
 use raw_window_handle::{HasRawDisplayHandle, HasRawWindowHandle};
 impl Renderer {
-    fn render(mut self) -> impl FnMut(){//impl Future<Output = ()> + Send + 'static{
-         move || {
-            println!("starting renderer");
-            if (self.rx.has_changed().unwrap()) {
-                println!("recieved command");
-                match match *self.rx.borrow_and_update() {
-                    RendererInput::Render => {
+    fn render(&mut self){//impl Future<Output = ()> + Send + 'static{
+                match {
                         let output = self.gpu.surface.get_current_texture().unwrap();
                         let view = output
                             .texture
@@ -158,12 +123,7 @@ impl Renderer {
                         self.gpu.queue.submit(std::iter::once(encoder.finish()));
                         output.present();
                         Ok(())
-                    }
-                    RendererInput::Resize(size) => {
-                        self.gpu.resize(size);
-                        Ok(())
-                    }
-                } {
+                    }{
                     Ok(_) => {}
                     Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
                         self.gpu.resize(self.gpu.size)
@@ -172,17 +132,13 @@ impl Renderer {
                         println!("render broke");
                     }
                     Err(e) => eprintln!("{:?}", e),
-                }
+                };
             }
-            println!("done rendering");
             // BoxFuture
-            }
-    }
-    fn new<W>(
+            
+fn new<W>(
         window: Arc<W>,
         size: impl Into<[u32; 2]>,
-        rx: Receiver<RendererInput>,
-        notify: Arc<Notify>,
     ) -> impl Future<Output = Self> + Send + 'static
     where
         W: HasRawWindowHandle + HasRawDisplayHandle + Send + Sync + 'static,
@@ -268,8 +224,6 @@ impl Renderer {
                 index_buffer,
                 num_indices,
                 num_vertices,
-                rx,
-                notify, // window: window_handler,
             }
         }
     }
@@ -349,26 +303,47 @@ fn main() {
     // let window_handler = WindowHandler{window,event_loop};
     // let window_handler = window_handler;
     let (tx, mut rx) = watch::channel(RendererInput::Render);
-    let render_notify = Arc::new(Notify::new());
     // window_handler.window.as_ref
-    let renderer_task = task::spawn(Renderer::new(
+    let mut renderer = tokio_runtime.block_on(Renderer::new(
         window_handler.window.clone(),
         *(&window_handler.get_size()),
-        rx,
-        render_notify.clone(),
-    ));
+         ));
     let mut time = std::time::Instant::now();
     let mut dt = std::time::Duration::ZERO;
     // tx.send(RendererInput::Render).unwrap();
     let WindowHandler { window, event_loop } = window_handler;
-
-    let mut renderer = tokio_runtime.block_on(renderer_task).unwrap();
-    let mut render_task = Box::pin(renderer.render());
+    let notify = Arc::new(Notify::new());
+    let notify2 = notify.clone();
+    tokio_runtime.block_on(async{
+        tokio_runtime.spawn(async move{
+            loop{
+                if rx.has_changed().unwrap(){
+                    println!("has changed");
+                    match *rx.borrow_and_update(){
+                        RendererInput::Render =>{
+                            renderer.render();
+                        },
+                        RendererInput::Resize(size)=>{
+                            renderer.gpu.resize(size);
+                        },
+                        RendererInput::Exit=>{
+                            break;
+                        }
+                    };
+                }
+                notify.notify_one();
+                task::yield_now().await;
+            }
+            notify.notify_one();
+        });
+    });
     // tokio::pin!(render_task);
+    let mut last_frame = std::time::Duration::ZERO;
     event_loop.run(move |event, _, control_flow| {
         dt = std::time::Instant::now() - time;
         time += dt;
-        // println!("{:?}", dt);
+        last_frame += dt;
+        println!("{:?}", last_frame);
         match event{
             Event::WindowEvent {
                 ref event,
@@ -387,7 +362,10 @@ fn main() {
                                 ..
                             },
                         ..
-                    } => *control_flow = ControlFlow::Exit,
+                    } => {
+                        tx.send(RendererInput::Exit).unwrap();
+                        *control_flow = ControlFlow::Exit
+                    },
                     WindowEvent::Resized(physical_size) => {
                         tx.send(RendererInput::Resize((*physical_size).into())).unwrap();
                         // gpu.resize(*physical_size);
@@ -403,18 +381,15 @@ fn main() {
             Event::RedrawRequested(window_id)
                 if window_id == window.id() =>
             {
-
-                match tx.send(RendererInput::Render){
-                    Ok(_) => {}
-                    Err(error) =>{ print!("error")}
-                };
-                // let x = renderfn();
+                tx.send(RendererInput::Render).unwrap();
+                tokio_runtime.block_on(notify2.notified());
+                last_frame = std::time::Duration::ZERO;
+                        // let x = renderfn();
                 // let render_task = tokio_runtime.spawn((&mut render_task));
                 
                 // {
                 // let render_task = tokio_runtime.block_on(render_task);
                 // let render_task = tokio_runtime.spawn(renderer.run());
-                tokio_runtime.block_on(async{render_task.as_mut()});
                 // let renderer = tokio_runtime.block_on(render_task);
                 // }
                 // pollster::block_on(render_notify.notified());
@@ -423,9 +398,11 @@ fn main() {
                  // let render = task::spawn(render(&gpu,&render_pipeline,&vertex_buffer ,&num_vertices ,&index_buffer,&num_indices));
                 // pollster::block_on(render);
              },Event::MainEventsCleared =>{
-                window.request_redraw();
 
-            }
+                if last_frame.as_millis() >= 16{
+                    window.request_redraw();
+                }
+         }   
             _ => {}
         }
         // event_loop.run(test);
